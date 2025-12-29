29a2d2fd4526b32a655d761f26bec11e84f43409
maksim.samorukov <maxim.samorukov@gmail.com>
2025-12-29 23:28:21 +0300

    upload wip

--- src/components/FileUpload/index.tsx
+++ src/components/FileUpload/index.tsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Card, Text } from "@radix-ui/themes";
 import s from "./style.module.scss";
-import { ArrowUpFromLine } from "lucide-react";
+import { ArrowUpFromLine, FileText, X } from "lucide-react";
 
 type FileUploadProps = {};
-
+const MAX = 6;
 export const FileUpload: React.FC<FileUploadProps> = () => {
+  const [fileItems, setFileItems] = useState<FileList | null>(null);
+  const ref = useRef<HTMLInputElement>(null);
+  const onUpload = () => {
+    if (ref.current) {
+      ref.current.click();
+    }
+  };
+  const handleUpload: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    console.log(e.target.files);
+    setFileItems(e.target.files);
+  };
+  const handleRemoveFiles = () => {
+    setFileItems(null);
+  };
   return (
     <div className={s.container}>
       <Card variant="surface" className={s.uploadContainer}>
-        <button className={s.upload}>
+        <input
+          disabled={(fileItems?.length || 0) >= MAX}
+          onChange={handleUpload}
+          ref={ref}
+          style={{ display: "none" }}
+          type="file"
+          multiple
+          max={6}
+        />
+        <button onClick={onUpload} className={s.upload}>
           <ArrowUpFromLine />
         </button>
       </Card>
-      <Text className={s.text}>Прикрепите до 6 файлов</Text>
+      <div className={s.dataContainer}>
+        <Text className={s.text}>Прикрепите до {MAX} файлов</Text>
+        <div className={s.files}>
+          {Array.from({ length: fileItems?.length || 0 }).map(() => (
+            <FileText color="#5e5c64" />
+          ))}
+          {(fileItems?.length || 0) > 0 && (
+            <button onClick={handleRemoveFiles} className={s.removeButton}>
+              <X color="#e01b24" />
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
