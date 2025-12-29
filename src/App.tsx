a89af8397afa3c2fc607eb90f040b90430734917
maksim.samorukov <maxim.samorukov@gmail.com>
2025-12-30 01:06:14 +0300

    policy

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,10 @@ import { SubmitButton } from "./components/Submit";
 import { Footer } from "./components/Footer";
 import { initialState, reducer, type StateType } from "./reducer";
 import s from "./App.module.scss";
+import { Policy } from "./components/Policy";
 
 function App() {
+  const [showPolicy, setShowPolicy] = useState(false);
   const [state, dispatch] = useReducer(reducer, initialState);
   const [formValid, setFormValid] = useState({
     name: true,
@@ -51,57 +53,64 @@ function App() {
   ) => {
     dispatch({ type, payload: { value } });
   };
+  const handleBack = useCallback(() => setShowPolicy(false), []);
+  const handleShowPolicy = useCallback(() => setShowPolicy(true), []);
   return (
     <Card className={s.container} variant="surface" size={"2"}>
-      <Flex direction={"column"} align={"start"} justify={"start"} gap={"5"}>
-        <Header title="Оставить заявку на расчет" />
-        <Flex
-          style={{ width: "100%" }}
-          direction={"column"}
-          align={"start"}
-          justify={"start"}
-          gap={"5"}
-        >
-          <InputField
-            handleValidation={handleFormValidation}
-            value={state["name"]}
-            handleInput={onInput}
-            type="name"
-            required
-            placeholder="Ваше имя"
+      {showPolicy ? (
+        <Policy onBack={handleBack} />
+      ) : (
+        <Flex direction={"column"} align={"start"} justify={"start"} gap={"5"}>
+          <Header title="Оставить заявку на расчет" />
+          <Flex
+            style={{ width: "100%" }}
+            direction={"column"}
+            align={"start"}
+            justify={"start"}
+            gap={"5"}
+          >
+            <InputField
+              handleValidation={handleFormValidation}
+              value={state["name"]}
+              handleInput={onInput}
+              type="name"
+              required
+              placeholder="Ваше имя"
+            />
+            <InputField
+              handleValidation={handleFormValidation}
+              value={state["phoneNumber"]}
+              handleInput={onInput}
+              type="phoneNumber"
+              required
+              placeholder="Телефон"
+            />
+            <InputField
+              handleValidation={handleFormValidation}
+              value={state["email"]}
+              handleInput={onInput}
+              type="email"
+              required={false}
+              placeholder="E-mail"
+            />
+            <InputField
+              handleValidation={handleFormValidation}
+              value={state["message"]}
+              handleInput={onInput}
+              type="message"
+              required={false}
+              placeholder="Сообщение"
+            />
+          </Flex>
+          <FileUpload handleInput={onInput} />
+          <SubmitButton
+            showPolicy={handleShowPolicy}
+            disabled={submitBtnDisabled}
+            handleSubmit={handleSubmit}
           />
-          <InputField
-            handleValidation={handleFormValidation}
-            value={state["phoneNumber"]}
-            handleInput={onInput}
-            type="phoneNumber"
-            required
-            placeholder="Телефон"
-          />
-          <InputField
-            handleValidation={handleFormValidation}
-            value={state["email"]}
-            handleInput={onInput}
-            type="email"
-            required={false}
-            placeholder="E-mail"
-          />
-          <InputField
-            handleValidation={handleFormValidation}
-            value={state["message"]}
-            handleInput={onInput}
-            type="message"
-            required={false}
-            placeholder="Сообщение"
-          />
+          <Footer />
         </Flex>
-        <FileUpload handleInput={onInput} />
-        <SubmitButton
-          disabled={submitBtnDisabled}
-          handleSubmit={handleSubmit}
-        />
-        <Footer />
-      </Flex>
+      )}
     </Card>
   );
 }
--- src/components/Submit/index.tsx
+++ src/components/Submit/index.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { Button, Link, Text } from "@radix-ui/themes";
+import { Button, Text } from "@radix-ui/themes";
 import s from "./style.module.scss";
 
 type SubmitProps = {
   handleSubmit: () => void;
   disabled: boolean;
+  showPolicy: () => void;
 };
 
 export const SubmitButton: React.FC<SubmitProps> = ({
   disabled,
   handleSubmit,
+  showPolicy,
 }) => {
   return (
     <div className={s.container}>
       <Text className={s.text} color="gray">
         Нажимая на кнопку «Отправить», Вы принимаете условия{" "}
-        <Link href="#">политики конфиденциальности</Link> в отношении обработки
-        персональных данных.
+        <button className={s.btnPolicy} onClick={showPolicy}>
+          политики конфиденциальности
+        </button>{" "}
+        в отношении обработки персональных данных.
       </Text>
       <div className={s.submitContainer}>
         <Button disabled={disabled} onClick={handleSubmit} className={s.submit}>
